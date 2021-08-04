bc24b3028f449bcabf87a329b60a65e82de43c03
keithkirui91 <keith.kirui33@gmail.com>
2021-08-04 14:53:59 +0300

    update files
    Add a hover effect on the images
    add style

--- products.js
+++ products.js
@@ -31,17 +31,17 @@ var shoeFiveE = document.getElementById("shoeFiveE");
 var shoeFiveF = document.getElementById("shoeFiveF");
 
 //underarmour
-var shoeSixA = document.getElementById("shoeSixA");
-var shoeSixB = document.getElementById("shoeSixB");
-var shoeSixC = document.getElementById("shoeSixC");
+var shoeFiveA = document.getElementById("shoeFiveA");
+var shoeFiveB = document.getElementById("shoeFiveB");
+var shoeFiveC = document.getElementById("shoeFiveC");
 
 //clarks
-var shoeSevenA= document.getElementById("shoeSevenA");
-var shoeSevenB= document.getElementById("shoeSevenB");
-var shoeSevenC= document.getElementById("shoeSevenC");
-var shoeSevenD= document.getElementById("shoeSevenD");
-var shoeSevenE= document.getElementById("shoeSevenE");
-var shoeSevenF= document.getElementById("shoeSevenF");
+var shoeSixA= document.getElementById("shoeSixA");
+var shoeSixB= document.getElementById("shoeSixB");
+var shoeSixC= document.getElementById("shoeSixC");
+var shoeSixD= document.getElementById("shoeSixD");
+var shoeSixE= document.getElementById("shoeSixE");
+var shoeSixF= document.getElementById("shoeSixF");
 
 
 
@@ -100,123 +100,528 @@ shoeSevenF.style.display = "none";
 
 
 
-/*puma-marathon*/
 
-function shoeOneIn(){
-  if(shoeOne.style.display = "none"){
-    shoeOne.style.display = "block";
-  }
-  else{
-    shoeOne.style.display = "none"
-  }
-}
 
-function shoeOneOut(){
-  if (shoeOne.style.display = "block"){
-    shoeOne.style. display = "none";
-  }
-}
 
+/*pumaMARATHON*/
+//A
+function shoeOneAIn(){
+    if(shoeOneA.style.display = "none"){
+      shoeOneA.style.display = "block";
+    }
+    else{
+      shoeOneA.style.display = "none"
+    }
+  }
+  
+  function shoeOneAOut(){
+    if (shoeOneA.style.display = "block"){
+      shoeOneA.style. display = "none";
+    }
+  }
+  //B
+
+  function shoeOneBIn(){
+    if(shoeOneB.style.display = "none"){
+      shoeOneB.style.display = "block";
+    }
+    else{
+      shoeOneB.style.display = "none"
+    }
+  }
+  
+  function shoeOneBOut(){
+    if (shoeOneB.style.display = "block"){
+      shoeOneB.style. display = "none";
+    }
+  }
+
+  //C
+  function shoeOneCIn(){
+    if(shoeOneC.style.display = "none"){
+      shoeOneC.style.display = "block";
+    }
+    else{
+      shoeOneC.style.display = "none"
+    }
+  }
+  
+  function shoeOneCOut(){
+    if (shoeOneC.style.display = "block"){
+      shoeOneC.style. display = "none";
+    }
+  }
 
 /*puma*/
+//A
+function shoeTwoAIn(){
+  if(shoeTwoA.style.display = "none"){
+    shoeTwoA.style.display = "block";
+  }
+  else{
+    shoeTwoA.style.display = "none"
+  }
+}
 
-function shoeOneIn(){
-    if(shoeOne.style.display = "none"){
-      shoeOne.style.display = "block";
+function shoeTwoAOut(){
+  if (shoeTwoA.style.display = "block"){
+    shoeTwoA.style. display = "none";
+  }
+}
+//B
+function shoeTwoBIn(){
+    if(shoeTwoB.style.display = "none"){
+      shoeTwoB.style.display = "block";
     }
     else{
-      shoeOne.style.display = "none"
+      shoeTwoB.style.display = "none"
     }
   }
   
-  function shoeOneOut(){
-    if (shoeOne.style.display = "block"){
-      shoeOne.style. display = "none";
+  function shoeTwoBOut(){
+    if (shoeTwoB.style.display = "block"){
+      shoeTwoB.style. display = "none";
     }
   }
 
+//C
+function shoeTwoCIn(){
+    if(shoeTwoC.style.display = "none"){
+      shoeTwoC.style.display = "block";
+    }
+    else{
+      shoeTwoC.style.display = "none"
+    }
+  }
+  
+  function shoeTwoCOut(){
+    if (shoeTwoC.style.display = "block"){
+      shoeTwoC.style. display = "none";
+    }
+  }
+  
+  
 /*adidas*/
-
-function shoeOneIn(){
-    if(shoeOne.style.display = "none"){
-      shoeOne.style.display = "block";
+//A
+function shoeThreeAIn(){
+    if(shoeThreeA.style.display = "none"){
+      shoeThreeA.style.display = "block";
     }
     else{
-      shoeOne.style.display = "none"
+      shoeThreeA.style.display = "none"
     }
   }
   
-  function shoeOneOut(){
-    if (shoeOne.style.display = "block"){
-      shoeOne.style. display = "none";
+  function shoeThreeAOut(){
+    if (shoeThreeA.style.display = "block"){
+      shoeThreeA.style. display = "none";
     }
   }
-
-
-  /*vans*/
-
-function shoeOneIn(){
-    if(shoeOne.style.display = "none"){
-      shoeOne.style.display = "block";
+  //B
+  function shoeThreeBIn(){
+    if(shoeThreeB.style.display = "none"){
+      shoeThreeB.style.display = "block";
     }
     else{
-      shoeOne.style.display = "none"
+      shoeThreeB.style.display = "none"
     }
   }
   
-  function shoeOneOut(){
-    if (shoeOne.style.display = "block"){
-      shoeOne.style. display = "none";
+  function shoeThreeBOut(){
+    if (shoeThreeB.style.display = "block"){
+      shoeThreeB.style. display = "none";
     }
   }
+  
+  //c
+  function shoeThreeCIn(){
+    if(shoeThreeC.style.display = "none"){
+      shoeThreeC.style.display = "block";
+    }
+    else{
+      shoeThreeC.style.display = "none"
+    }
+  }
+  
+  function shoeThreeCOut(){
+    if (shoeThreeC.style.display = "block"){
+      shoeThreeC.style. display = "none";
+    }
+  }
+  //d
+  function shoeThreeDIn(){
+  if(shoeThreeD.style.display = "none"){
+    shoeThreeD.style.display = "block";
+  }
+  else{
+    shoeThreeD.style.display = "none"
+  }
+}
+
+function shoeThreeDOut(){
+  if (shoeThreeD.style.display = "block"){
+    shoeThreeD.style. display = "none";
+  }
+}
+//e
+function shoeThreeEIn(){
+    if(shoeThreeE.style.display = "none"){
+      shoeThreeE.style.display = "block";
+    }
+    else{
+      shoeThreeE.style.display = "none"
+    }
+  }
+  
+  function shoeThreeEOut(){
+    if (shoeThreeE.style.display = "block"){
+      shoeThreeE.style. display = "none";
+    }
+  }
+  //f
+  function shoeThreeFIn(){
+    if(shoeThreeF.style.display = "none"){
+      shoeThreeF.style.display = "block";
+    }
+    else{
+      shoeThreeF.style.display = "none"
+    }
+  }
+  
+  function shoeThreeFOut(){
+    if (shoeThreeF.style.display = "block"){
+      shoeThreeF.style. display = "none";
+    }
+  }
+  
+
+/*vans*/
+//a
+function shoeFourAIn(){
+    if(shoeFourA.style.display = "none"){
+      shoeFourA.style.display = "block";
+    }
+    else{
+      shoeFourA.style.display = "none"
+    }
+  }
+  
+  function shoeFourAOut(){
+    if (shoeFourA.style.display = "block"){
+      shoeFourA.style. display = "none";
+    }
+  }
+  //b
+  function shoeFourBIn(){
+    if(shoeFourB.style.display = "none"){
+      shoeFourB.style.display = "block";
+    }
+    else{
+      shoeFourB.style.display = "none"
+    }
+  }
+  
+  function shoeFourBOut(){
+    if (shoeFourB.style.display = "block"){
+      shoeFourB.style. display = "none";
+    }
+  }
+  
+  //C
+  function shoeFourCIn(){
+    if(shoeFourC.style.display = "none"){
+      shoeFourC.style.display = "block";
+    }
+    else{
+      shoeFourC.style.display = "none"
+    }
+  }
+  
+  function shoeFourCOut(){
+    if (shoeFourC.style.display = "block"){
+      shoeFourC.style. display = "none";
+    }
+  }
+  //D
+  function shoeFourDIn(){
+    if(shoeFourD.style.display = "none"){
+      shoeFourD.style.display = "block";
+    }
+    else{
+      shoeFourD.style.display = "none"
+    }
+  }
+  
+  function shoeFourDOut(){
+    if (shoeFourD.style.display = "block"){
+      shoeFourD.style. display = "none";
+    }
+  }
+  //E
+  function shoeFourEIn(){
+    if(shoeFourE.style.display = "none"){
+      shoeFourE.style.display = "block";
+    }
+    else{
+      shoeFourE.style.display = "none"
+    }
+  }
+  
+  function shoeFourEOut(){
+    if (shoeFourE.style.display = "block"){
+      shoeFourE.style. display = "none";
+    }
+  }
+  //F
+  function shoeFourFIn(){
+    if(shoeFourF.style.display = "none"){
+      shoeFourF.style.display = "block";
+    }
+    else{
+      shoeFourF.style.display = "none"
+    }
+  }
+  
+  function shoeFourFOut(){
+    if (shoeFourF.style.display = "block"){
+      shoeFourF.style. display = "none";
+    }
+  }
+  
 
   /*converse*/
-
-function shoeOneIn(){
-    if(shoeOne.style.display = "none"){
-      shoeOne.style.display = "block";
+//A
+  function shoeFiveAIn(){
+    if(shoeFiveA.style.display = "none"){
+      shoeFiveA.style.display = "block";
     }
     else{
-      shoeOne.style.display = "none"
+      shoeFiveA.style.display = "none"
     }
   }
   
-  function shoeOneOut(){
-    if (shoeOne.style.display = "block"){
-      shoeOne.style. display = "none";
+  function shoeFiveAOut(){
+    if (shoeFiveA.style.display = "block"){
+      shoeFiveA.style. display = "none";
     }
   }
 
+ //B
+ function shoeFiveBIn(){
+    if(shoeFiveB.style.display = "none"){
+      shoeFiveB.style.display = "block";
+    }
+    else{
+      shoeFiveB.style.display = "none"
+    }
+  }
+  
+  function shoeFiveBOut(){
+    if (shoeFiveB.style.display = "block"){
+      shoeFiveB.style. display = "none";
+    }
+  }
+   //C
+   function shoeFiveCIn(){
+    if(shoeFiveC.style.display = "none"){
+      shoeFiveC.style.display = "block";
+    }
+    else{
+      shoeFiveC.style.display = "none"
+    }
+  }
+  
+  function shoeFiveCOut(){
+    if (shoeFiveC.style.display = "block"){
+      shoeFiveC.style. display = "none";
+    }
+  }
+  //D
+  function shoeFiveDIn(){
+    if(shoeFiveD.style.display = "none"){
+      shoeFiveD.style.display = "block";
+    }
+    else{
+      shoeFiveD.style.display = "none"
+    }
+  }
+  
+  function shoeFiveDOut(){
+    if (shoeFiveD.style.display = "block"){
+      shoeFiveD.style. display = "none";
+    }
+  }
+  //E
+  function shoeFiveEIn(){
+    if(shoeFiveE.style.display = "none"){
+      shoeFiveE.style.display = "block";
+    }
+    else{
+      shoeFiveE.style.display = "none"
+    }
+  }
+  
+  function shoeFiveEOut(){
+    if (shoeFiveE.style.display = "block"){
+      shoeFiveE.style. display = "none";
+    }
+  }
+  //F
+  function shoeFiveFIn(){
+    if(shoeFiveF.style.display = "none"){
+      shoeFiveF.style.display = "block";
+    }
+    else{
+      shoeFiveF.style.display = "none"
+    }
+  }
+  
+  function shoeFiveFOut(){
+    if (shoeFiveF.style.display = "block"){
+      shoeFiveF.style. display = "none";
+    }
+  }
+  
   /*underarmour*/
-
-function shoeOneIn(){
-    if(shoeOne.style.display = "none"){
-      shoeOne.style.display = "block";
+//A
+  function shoeSixAIn(){
+    if(shoeSixA.style.display = "none"){
+      shoeSixA.style.display = "block";
     }
     else{
-      shoeOne.style.display = "none"
+      shoeSixA.style.display = "none"
     }
   }
   
-  function shoeOneOut(){
-    if (shoeOne.style.display = "block"){
-      shoeOne.style. display = "none";
+  function shoeSixAOut(){
+    if (shoeSixA.style.display = "block"){
+      shoeSixA.style. display = "none";
+    }
+  }
+//B
+
+function shoeSixBIn(){
+    if(shoeSixB.style.display = "none"){
+      shoeSixB.style.display = "block";
+    }
+    else{
+      shoeSixB.style.display = "none"
+    }
+  }
+  
+  function shoeSixBOut(){
+    if (shoeSixB.style.display = "block"){
+      shoeSixB.style. display = "none";
+    }
+  }
+  
+//C
+function shoeSixCIn(){
+    if(shoeSixC.style.display = "none"){
+      shoeSixC.style.display = "block";
+    }
+    else{
+      shoeSixC.style.display = "none"
+    }
+  }
+  
+  function shoeSixCOut(){
+    if (shoeSixC.style.display = "block"){
+      shoeSixC.style. display = "none";
     }
   }
 
   /*clarks*/
-
-function shoeOneIn(){
-    if(shoeOne.style.display = "none"){
-      shoeOne.style.display = "block";
+  //A
+  function shoeSevenAIn(){
+    if(shoeSevenA.style.display = "none"){
+      shoeSevenA.style.display = "block";
     }
     else{
-      shoeOne.style.display = "none"
+      shoeSevenA.style.display = "none"
     }
   }
   
-  function shoeOneOut(){
-    if (shoeOne.style.display = "block"){
-      shoeOne.style. display = "none";
+  function shoeSevenAOut(){
+    if (shoeSevenA.style.display = "block"){
+      shoeSevenA.style. display = "none";
+    }
+  }
+
+//B
+function shoeSevenBIn(){
+    if(shoeSevenB.style.display = "none"){
+      shoeSevenB.style.display = "block";
+    }
+    else{
+      shoeSevenB.style.display = "none"
+    }
+  }
+  
+  function shoeSevenBOut(){
+    if (shoeSevenB.style.display = "block"){
+      shoeSevenB.style. display = "none";
+    }
+  }
+//C
+function shoeSevenCIn(){
+    if(shoeSevenC.style.display = "none"){
+      shoeSevenC.style.display = "block";
+    }
+    else{
+      shoeSevenC.style.display = "none"
+    }
+  }
+  
+  function shoeSevenCOut(){
+    if (shoeSevenC.style.display = "block"){
+      shoeSevenC.style. display = "none";
+    }
+  }
+
+//D
+function shoeSevenDIn(){
+    if(shoeSevenD.style.display = "none"){
+      shoeSevenD.style.display = "block";
+    }
+    else{
+      shoeSevenD.style.display = "none"
+    }
+  }
+  
+  function shoeSevenDOut(){
+    if (shoeSevenD.style.display = "block"){
+      shoeSevenD.style. display = "none";
+    }
+  }
+//E
+  function shoeSevenEIn(){
+    if(shoeSevenE.style.display = "none"){
+      shoeSevenE.style.display = "block";
+    }
+    else{
+      shoeSevenE.style.display = "none"
+    }
+  }
+  
+  function shoeSevenEOut(){
+    if (shoeSevenE.style.display = "block"){
+      shoeSevenE.style. display = "none";
+    }
+  }
+
+//F
+function shoeSevenFIn(){
+    if(shoeSevenF.style.display = "none"){
+      shoeSevenF.style.display = "block";
+    }
+    else{
+      shoeSevenF.style.display = "none"
+    }
+  }
+  
+  function shoeSevenFOut(){
+    if (shoeSevenF.style.display = "block"){
+      shoeSevenF.style. display = "none";
     }
   }
