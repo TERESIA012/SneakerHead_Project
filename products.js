213416909debc2063b55691a380510381064b37b
keithkirui91 <keith.kirui33@gmail.com>
2021-08-04 11:27:42 +0300

    update .js
    Add white box variables

---
+++ products.js
@@ -0,0 +1,222 @@
+/*shoe WHITE BOX */
+//puma marathon
+var shoeOneA = document.getElementById("shoeOneA");
+var shoeOneB = document.getElementById("shoeOneB");
+var shoeOneC = document.getElementById("shoeOneC");
+//puma
+var shoeTwoA = document.getElementById("shoeTwoA");
+var shoeTwoB = document.getElementById("shoeTwoB");
+var shoeTwoC = document.getElementById("shoeTwoC");
+//adidas
+var shoeThreeA = document.getElementById("shoeThreeA");
+var shoeThreeB = document.getElementById("shoeThreeB");
+var shoeThreeC = document.getElementById("shoeThreeC");
+var shoeThreeD = document.getElementById("shoeThreeD");
+var shoeThreeE = document.getElementById("shoeThreeE");
+var shoeThreeF = document.getElementById("shoeThreeF");
+//vans
+
+var shoeFourA = document.getElementById("shoeFourA");
+var shoeFourB = document.getElementById("shoeFourB");
+var shoeFourC = document.getElementById("shoeFourC");
+var shoeFourD = document.getElementById("shoeFourD");
+var shoeFourE = document.getElementById("shoeFourE");
+var shoeFourF = document.getElementById("shoeFourF");
+//converse
+var shoeFiveA = document.getElementById("shoeFiveA");
+var shoeFiveB = document.getElementById("shoeFiveB");
+var shoeFiveC = document.getElementById("shoeFiveC");
+var shoeFiveD = document.getElementById("shoeFiveD");
+var shoeFiveE = document.getElementById("shoeFiveE");
+var shoeFiveF = document.getElementById("shoeFiveF");
+
+//underarmour
+var shoeSixA = document.getElementById("shoeSixA");
+var shoeSixB = document.getElementById("shoeSixB");
+var shoeSixC = document.getElementById("shoeSixC");
+
+//clarks
+var shoeSevenA= document.getElementById("shoeSevenA");
+var shoeSevenB= document.getElementById("shoeSevenB");
+var shoeSevenC= document.getElementById("shoeSevenC");
+var shoeSevenD= document.getElementById("shoeSevenD");
+var shoeSevenE= document.getElementById("shoeSevenE");
+var shoeSevenF= document.getElementById("shoeSevenF");
+
+
+
+//puma marathon
+shoeOneA.style.display = "none";
+shoeOneB.style.display = "none";
+shoeOneC.style.display = "none";
+
+
+//puma
+shoeTwoA.style.display = "none";
+shoeTwoB.style.display = "none";
+shoeTwoC.style.display = "none";
+
+
+
+//adidas
+shoeThreeA.style.display = "none";
+shoeThreeB.style.display = "none";
+shoeThreeC.style.display = "none";
+shoeThreeD.style.display = "none";
+shoeThreeE.style.display = "none";
+shoeThreeF.style.display = "none";
+
+//vans
+shoeFourA.style.display = "none";
+shoeFourB.style.display = "none";
+shoeFourC.style.display = "none";
+shoeFourD.style.display = "none";
+shoeFourE.style.display = "none";
+shoeFourF.style.display = "none";
+
+
+
+//converse
+shoeFiveA.style.display = "none";
+shoeFiveB.style.display = "none";
+shoeFiveC.style.display = "none";
+shoeFiveD.style.display = "none";
+shoeFiveE.style.display = "none";
+shoeFiveF.style.display = "none";
+
+//underarmour
+shoeSixA.style.display = "none";
+shoeSixB.style.display = "none";
+shoeSixC.style.display = "none";
+//clarks
+shoeSevenA.style.display = "none";
+shoeSevenB.style.display = "none";
+shoeSevenC.style.display = "none";
+shoeSevenD.style.display = "none";
+shoeSevenE.style.display = "none";
+shoeSevenF.style.display = "none";
+
+
+
+
+
+/*puma-marathon*/
+
+function shoeOneIn(){
+  if(shoeOne.style.display = "none"){
+    shoeOne.style.display = "block";
+  }
+  else{
+    shoeOne.style.display = "none"
+  }
+}
+
+function shoeOneOut(){
+  if (shoeOne.style.display = "block"){
+    shoeOne.style. display = "none";
+  }
+}
+
+
+/*puma*/
+
+function shoeOneIn(){
+    if(shoeOne.style.display = "none"){
+      shoeOne.style.display = "block";
+    }
+    else{
+      shoeOne.style.display = "none"
+    }
+  }
+  
+  function shoeOneOut(){
+    if (shoeOne.style.display = "block"){
+      shoeOne.style. display = "none";
+    }
+  }
+
+/*adidas*/
+
+function shoeOneIn(){
+    if(shoeOne.style.display = "none"){
+      shoeOne.style.display = "block";
+    }
+    else{
+      shoeOne.style.display = "none"
+    }
+  }
+  
+  function shoeOneOut(){
+    if (shoeOne.style.display = "block"){
+      shoeOne.style. display = "none";
+    }
+  }
+
+
+  /*vans*/
+
+function shoeOneIn(){
+    if(shoeOne.style.display = "none"){
+      shoeOne.style.display = "block";
+    }
+    else{
+      shoeOne.style.display = "none"
+    }
+  }
+  
+  function shoeOneOut(){
+    if (shoeOne.style.display = "block"){
+      shoeOne.style. display = "none";
+    }
+  }
+
+  /*converse*/
+
+function shoeOneIn(){
+    if(shoeOne.style.display = "none"){
+      shoeOne.style.display = "block";
+    }
+    else{
+      shoeOne.style.display = "none"
+    }
+  }
+  
+  function shoeOneOut(){
+    if (shoeOne.style.display = "block"){
+      shoeOne.style. display = "none";
+    }
+  }
+
+  /*underarmour*/
+
+function shoeOneIn(){
+    if(shoeOne.style.display = "none"){
+      shoeOne.style.display = "block";
+    }
+    else{
+      shoeOne.style.display = "none"
+    }
+  }
+  
+  function shoeOneOut(){
+    if (shoeOne.style.display = "block"){
+      shoeOne.style. display = "none";
+    }
+  }
+
+  /*clarks*/
+
+function shoeOneIn(){
+    if(shoeOne.style.display = "none"){
+      shoeOne.style.display = "block";
+    }
+    else{
+      shoeOne.style.display = "none"
+    }
+  }
+  
+  function shoeOneOut(){
+    if (shoeOne.style.display = "block"){
+      shoeOne.style. display = "none";
+    }
+  }
